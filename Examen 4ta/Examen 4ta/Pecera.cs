38e48005e3260b80e6e5afecd04d6ac7d9d63701
Ericka Rocío Talé Aguilera <201800083@colsanjose.edu.gt>
2021-10-08 07:38:10 -0600

    examen

---
+++ Examen 4ta/Examen 4ta/Pecera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_4ta
+{
+    class Pecera
+    {
+        private double galones = 0;
+        private double litros = 0;
+
+        public void CalculoLitros(double largo, double ancho, double alto)
+        {
+            litros = largo * ancho * largo;
+        }
+
+        public double VerLitros()
+        {
+            return litros;
+        }
+
+        public double CalculoGalones()
+        {
+
+        }
+    }
+}
+    }
+}
---
+++ Examen 4ta/Examen 4ta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_4ta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             class Menu
+        {
+            public int MenuPecera()
+            {
+                Console.WriteLine("1.- Ingresar Datos");
+                Console.WriteLine("2.- Litros");
+                Console.WriteLine("3.- Galones");
+                Console.WriteLine("4.- Salir");
+                return int.TryParse();
+            }
+        }
+    }
+
+    
+
